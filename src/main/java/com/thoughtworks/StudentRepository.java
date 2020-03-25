c244ef78913396f7fe10e9554fa82ac8cffb81e0
Asuu <suqinzheng_0624@163.com>
2020-03-25 21:12:03 +0800

    [Asu] feat: search, select, update and delete students

--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -3,7 +3,9 @@ package com.thoughtworks;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,19 +37,84 @@ public class StudentRepository {
 
   public List<Student> query() {
     // TODO:
-    return new ArrayList<>();
+    Connection conn = DbUtil.getConnection();
+    List<Student> students = new ArrayList<>();
+    String sql = "SELECT id,name,gender,admission,birthday,class FROM student_info";
+    try {
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+      Student student = null;
+      while (rs.next()) {
+        student = new Student();
+        student.setId(rs.getString("id"));
+        student.setName(rs.getString("name"));
+        student.setGender(rs.getString("gender"));
+        student.setAdmissionYear(rs.getInt("admission"));
+        student.setBirthday(rs.getDate("birthday"));
+        student.setClassId(rs.getString("class"));
+        students.add(student);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return students;
   }
 
   public List<Student> queryByClassId(String classId) {
     // TODO:
-    return new ArrayList<>();
+    List<Student> students = new ArrayList<>();
+    Connection conn = DbUtil.getConnection();
+    String sql = "SELECT id,name,gender,admission,birthday,class FROM student_info WHERE class = ? ORDER BY id DESC";
+    try {
+      PreparedStatement ptmt = conn.prepareStatement(sql);
+      ptmt.setString(1,classId);
+      ResultSet rs = ptmt.executeQuery();
+      Student student = null;
+      while (rs.next()) {
+        student = new Student();
+        student.setId(rs.getString("id"));
+        student.setName(rs.getString("name"));
+        student.setGender(rs.getString("gender"));
+        student.setAdmissionYear(rs.getInt("admission"));
+        student.setBirthday(rs.getDate("birthday"));
+        student.setClassId(rs.getString("class"));
+        students.add(student);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return students;
   }
 
   public void update(String id, Student student) {
     // TODO:
+    Connection conn = DbUtil.getConnection();
+    String sql = "UPDATE student_info SET id = ?, name = ?, gender = ?, admission = ?,  birthday = ?, class = ? WHERE id = ?";
+    try {
+      PreparedStatement ptmt = conn.prepareStatement(sql);
+      ptmt.setString(1, student.getId());
+      ptmt.setString(2, student.getName());
+      ptmt.setString(3, student.getGender());
+      ptmt.setInt(4, student.getAdmissionYear());
+      ptmt.setDate(5, new Date(student.getBirthday().getTime()));
+      ptmt.setString(6, student.getClassId());
+      ptmt.setString(7,id);
+      ptmt.execute();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   public void delete(String id) {
     // TODO:
+    Connection conn = DbUtil.getConnection();
+    String sql = "DELETE FROM student_info WHERE id = ?";
+    try {
+      PreparedStatement ptmt = conn.prepareStatement(sql);
+      ptmt.setString(1,id);
+      ptmt.execute();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
